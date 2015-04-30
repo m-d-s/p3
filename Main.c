95652e56cc03e5597da8308731736cc9bd6ffc21
Michael Simpson <mds6@chandra.cs.pdx.edu>
2015-04-29 21:03:49 -0700

    Going to try a different direction

--- Main.c
+++ Main.c
@@ -10,7 +10,7 @@ code Main
   function main ()
       print ("          CS333 Project 3")
       print ("More Classic Synchronization Problems")
-      InitializeSheduler ()
+      InitializeScheduler ()
 
       SleepingBarber ()
 
@@ -22,10 +22,10 @@ code Main
 ------------------------- Sleeping Barber -----------------------------
 enum ENTER, SIT, BEGIN, FINISH, LEAVE, START, END      -- For display purposes
 var
-    bennys: BarberShop          
-    customers: array[20] of Thread = new array of Thread { 5 of new Thread }
+    bennys: BarberShop         
+    customers: array [20] of Thread = new array of Thread {20 of new Thread }
 
-function SeepingBarber ()
+function SleepingBarber ()
     var
       i: int
       j: int
@@ -34,43 +34,51 @@ function SeepingBarber ()
     bennys.Init ()                  -- Initialize the shop
    
     for i = 0 to 19
-        customer[i].Init ("C" + i)  --Initialize all customers
+        customers[i].Init ("C" + i)  --Initialize all customers
         endFor
 
     for i = 0 to 6
        for j = 0 to 19
-           customer[i].Fork ( checkTheShop, i )  -- Each customer tries to get seven haircuts
+           customers[i].Fork ( GoIntoTheShop, i )  -- Each customer tries to get seven haircuts
            endFor
        endFor
+    endFunction
+
+function GoIntoTheShop (i: int)
+
+    endFunction
+
+function GetHaircut () -- Args?
+
     endFunction
 
 
 class BarberShop
     superclass Object
     fields
-        barber: semaphore           -- Every shop needs a barber
-        chairs: semaphore           -- Where the customers wait
+        barber: Semaphore           -- Every shop needs a barber
+        chairs: Semaphore           -- Where the customers wait
         waiting: int                -- Keep track of how many customers are waiting
-        status: int                 -- For display purposes
- 
+        bStatus: int                 -- For display purposes
+        cStatus: array[20] of int
     methods
         Init ()                      -- Initialize barber related variables
 	OpenUpShop ()                -- Begin the day
         GiveHaircut ()               -- When you get a customer, cut their hair
-        BPrint ()                    -- Print barber status
-    endClass
+        PrintStatus ()                    -- Print barber status
+  endClass
+  behavior BarberShop
+    method Init ()
+        endMethod
 
+    method OpenUpShop ()
+        endMethod
 
-function checkTheShop (i: int)
+    method GiveHaircut ()
+        endMethod
 
-    endFunction
-
-function getHaircut () -- Args?
-
-    endFunction
-
-function CPrint()
-
-    endFunction
-
+    method PrintStatus ()
+        endMethod
+    
+    endBehavior  
 endCode
